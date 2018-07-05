76296e651a1eef0d69a4e16cac7c248fb32de1c9
Victor Almeida <38266337+victoralmeida432@users.noreply.github.com>
2018-07-04 23:58:08 -0300

    Update ProgMatAllInOne.py

--- ProgMatAllInOne.py
+++ ProgMatAllInOne.py
@@ -1,8 +1,6 @@
 from sympy import *
 from math import *
 
-
-
 def newton_Raphson():
 	
 	x=Symbol('x')
@@ -90,15 +88,61 @@ def La_Grange():
 	Px = fx1*L1+fx2*L2+fx3*L3
 	#Mostra o Resultado
 	print(Px)
+def Jacobi():
+	#input do número de interações que o usuário deseja
+	n = int(input('número de interações: '))
+
+	#input da matriz "y"
+	y1 = float(input('y1: '))
+	y2 = float(input('y2: '))
+	y3 = float(input('y3: '))
+
+	#input da matriz principal
+	a11 = float(input('a11: '))
+	a12 = float(input('a12: '))
+	a13 = float(input('a13: '))
+	a21 = float(input('a21: '))
+	a22 = float(input('a22: '))
+	a23 = float(input('a23: '))
+	a31 = float(input('a31: '))
+	a32 = float(input('a32: '))
+	a33 = float(input('a33: '))
+
+	k = 0 #variavel que será usada nas interações
+
+	while True: #loop para se fazer as equações de acordo com o número de interações que o usuário deseja
+
+	    X1 = (y1 - (a12*k) - (a13*k)) / a11
+	    X2 = (y2 - (a22*k) - (a23*k)) / a21
+	    X3 = (y3 - (a32*k) - (a33*k)) / a31
+	    print("X{} = ({};{};{})".format(k, X1, X2, X3))
+	    #variavel que irá ficar checando se chegou ao número de interações que o usuário colocou
+	    k = k + 1
+	    #se chegar ao número o loop para de rodar
+	    if k == n:
+	        break
 
 def main():
-	user_choice = int(input('Digite o metodo desejado: \n [1]Lagrange(Zeros de Função real) \n [2]Newton-Rapshon(Interpolação) \n [3]LU(Sistemas Lineares)\n\n'))
+	user_choice = int(input('Digite o metodo desejado: \n [1](Interpolação) \n [2](Zeros de Funcao Real) \n [3](Sistemas Lineares)\n \n'))
 	if user_choice == 1:
-		La_Grange()
+		userchoice = int(input('Quais metodos voce deseja ver \n [1]La_Grange \n [2]Newton \n'))
+		if userchoice == 1:
+			La_Grange()
+		else:
+			print('Erro, Metodo nao abordado pelo grupo')
+
 	elif user_choice == 2:
-		newton_Raphson()
+		userchoice = int(input('Quais metodos voce deseja ver \n [1]Bissecao \n [2]Newton-Raphson \n'))
+		if userchoice == 1:
+			print('Erro, Metodo nao abordado pelo grupo')
+		if userchoice ==2:
+			newton_Raphson()
 	elif user_choice == 3:
-		LU()
+		userchoice = int(input('Quais metodos voce deseja ver \n [1]LU \n [2]Jacobi \n'))
+		if userchoice == 1:
+   			LU()
+		if userchoice == 2:
+			Jacobi()	
 	else:
 		print('Erro, Metodo nao abordado pelo grupo')	
 if __name__ == "__main__":
